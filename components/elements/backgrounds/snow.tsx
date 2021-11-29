747f69bc045bf03ea649f4e0e007e2245fd86097
Frikk Jarl <Frikk@jarl.email>
2021-11-29 17:23:37 +0100

    Update
    wqrtq

--- components/elements/backgrounds/snow.tsx
+++ components/elements/backgrounds/snow.tsx
@@ -1,7 +1,7 @@
 
 export default function Snow({ }) {
     return (
-        <div className="absolute bottom-[-2px] left-0 z-1">
+        <div className="absolute bottom-[-2px] left-0 z-2">
             <svg width="100vw" preserveAspectRatio="none" viewBox="0 0 2159 204" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M1057.97 203.009C1100.5 203.009 1867.12 204 2159 204C1874.13 111.463 1521 -99.2002 1057.97 54.5108C574.39 215.044 477 -30.9909 0.5 203.009H1057.97Z" fill="#F6F6F6" />
             </svg>
