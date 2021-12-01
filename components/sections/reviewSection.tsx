07281567ba8f3de9707ae7a9977a7530442a6742
FRIKKern <32601161+FRIKKern@users.noreply.github.com>
2021-12-01 11:17:46 +0100

    s

--- components/sections/reviewSection.tsx
+++ components/sections/reviewSection.tsx
@@ -8,9 +8,9 @@ import reviewPicture from "@images/review-picture.png";
 export default function reviewSection({ title }) {
     return (
         <section className="text-white bg-[#002683] section-full ">
-            <div className="relative z-20 grid lg:grid-cols-12 ">
+            <div className="relative z-20 grid grid-cols-12 ">
 
-                <div className="mx-auto mt-[100px] max-w-3xl col-start-2 col-end-6 pb-[3rem] ">
+                <div className="mx-auto mt-[100px] max-w-3xl col-start-2 col-end-12 lg:col-end-6 pb-[3rem] ">
                     <h3 className="text-4xl font-semi mb-[2.5rem]">{title}</h3>
                     <p className="text-xl mb-[2.5rem] font-light">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede. </p>
                     <a className="text-xl font-light underline opacity-80 "> Se tidligere elevers progresjon og tilbakemeldinger</a>
--- components/sections/theSecrets.tsx
+++ components/sections/theSecrets.tsx
@@ -2,8 +2,8 @@
 
 export default function TheSecrets({ title }) {
     return (
-        <section className="relative z-20 grid lg:grid-cols-12 mb-[12.5rem] text-black section-full-contained">
-            <div className="col-span-4 lg:col-start-2 ">
+        <section className="relative z-20 grid grid-cols-12 mb-[12.5rem] text-black section-full-contained">
+            <div className="col-span-12 col-start-1 lg:col-span-4 lg:col-start-2 ">
                 <div className="p-[5rem] mt-[-3rem] w-[25rem] h-[25rem] rounded-full bg-white">
                     <h6 className="text-sm">Lær av dem beste</h6>
                     <h3 className="text-2xl">Sindre Wiig Nordby</h3>
@@ -19,7 +19,7 @@ export default function TheSecrets({ title }) {
                 </div>
 
             </div>
-            <div className="grid content-start max-w-2xl gap-10 lg:col-start-8 lg:col-end-12">
+            <div className="grid content-start max-w-2xl col-start-2 col-end-12 gap-10 mt-12 lg:mt-0 lg:col-start-8 lg:col-end-12">
                 <h3 className="text-4xl font-light">{title}</h3>
                 <p className="text-xl">
                     Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
